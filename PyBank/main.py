35a0dff48395afef2704e03e7a0a7d4c2cd4c1dc
asianhenry <henrybyoun@gmail.com>
2020-05-20 18:22:55 -0500

    added csv files, updates on final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
+#import modules
 import os
 import csv
 
 # Read in the CSV file
-csvpath = os.path.join('../../''budget_data.csv')
+csvpath = os.path.join('../csv_files','budget_data.csv')
 with open(csvpath) as csvfile:
 
     # Split the data on commas
@@ -10,28 +11,28 @@ with open(csvpath) as csvfile:
     #move to next line skip header
     header = next(csvreader)
 
-    
+    #create variables and empty lists we will need
     monthcount = 0
     totalprofit = 0
     monthlist = []
     profitlist = []
     profitlist2 = []
-    change = []
+    
     
     # Loop through the data
     for row in csvreader:
         #count the number of months(or rows)
         monthcount +=1
-        #add up all the values in profit/losses column
+        #add up all the values in profit/losses column to get the total profit
         totalprofit += int(row[1])
-       
-        #we create list for each column
+        #list with profit values
         profitlist.append(row[1])
-        #we create another list so we can calculate the change in profits
+        #list with profit values so we can calculate the change in profits
         profitlist2.append(row[1])
+        #list with months
         monthlist.append(row[0])
         
-#here we pop the first value so we can now subtract the values in profitlist from profitlist2        
+#here we pop the first value so we can now subtract the values in profitlist from profitlist2 to get the change between months      
 profitlist2.pop(0)
 
 #using this to format all the list elements as integers
@@ -44,8 +45,10 @@ for i in range(len(profitlist2)):
     profitlist2[i] = int(profitlist2[i])
 
 #now we can subtract the values from the previous month to find the change, then append to list 
-for i in range(len(profitlist2)):
-    change.append(profitlist2[i]-profitlist[i])
+#for i in range(len(profitlist2)):
+#    change.append(profitlist2[i]-profitlist[i])
+#^ let's clean this up and use comprehension
+change = [(profitlist2[i]-profitlist[i]) for i in range(len(profitlist2))]
 
 #calculate average change
 #round function to get 2 decimals
@@ -54,7 +57,6 @@ averagechange = round(sum(change) / len(change),2)
 maxincrease = max(change)
 maxdecrease = min(change)
 
-
 #find the corresponding month of the greatest increase/decrease in profits
 for i in range(len(change)):
     if change[i] == maxincrease:
@@ -62,23 +64,18 @@ for i in range(len(change)):
     elif change[i] == maxdecrease:
         lossmonth = monthlist[i+1]
 
-
-
 #print(profitlist)
 #print(profitlist2)
 #print(change)
-    
 
+#finally print results!    
 print("Financial Analysis")
 print("-------------------------")
 print(f"Total Months: {monthcount}")
 print(f"Total: ${totalprofit}")
 print(f"Average Change: ${averagechange}")
-
 print(f"Greatest Increase in Profits: {maxmonth} (${maxincrease})")
 print(f"Greatest Decrease in Profits: {lossmonth} (${maxdecrease})")
-
-
 
 
 #Financial Analysis
@@ -89,6 +86,8 @@ print(f"Greatest Decrease in Profits: {lossmonth} (${maxdecrease})")
 #Greatest Increase in Profits: Feb-2012 ($1926159)
 #Greatest Decrease in Profits: Sep-2013 ($-2196167)
 
+
+#write the code output into a text file
 with open("financial_analysis.txt", "w+") as f:
     print("Financial Analysis",file=f)
     print("-------------------------",file=f)
@@ -97,6 +96,4 @@ with open("financial_analysis.txt", "w+") as f:
     print(f"Average Change: ${averagechange}",file=f)
 
     print(f"Greatest Increase in Profits: {maxmonth} (${maxincrease})",file=f)
-    print(f"Greatest Decrease in Profits: {lossmonth} (${maxdecrease})",file=f)
-
-        
+    print(f"Greatest Decrease in Profits: {lossmonth} (${maxdecrease})",file=f)      
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,27 @@
-
+#import modules
 import os
 import csv
 
 # Read in the CSV file
-csvpath = os.path.join('../../''election_data.csv')
+csvpath = os.path.join('../csv_files','election_data.csv')
 with open(csvpath) as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
-    #variable for vote counts
+    #variable for vote counts, empty list for candidates
     totalvotes = 0
-
     candidate = []
     
-    # Loop through the data
+    #add up total votes (total rows), create list with candidate names
     for row in csvreader:
         totalvotes += 1
         candidate.append(row[2])
 
 #------------------------------------------------------------
 
-#O.O this creates a dictionary with the candidate names (keys) and the number of votes
+#O.O this creates a dictionary with the candidate names (keys) and the number of votes (values)
 #from collections import Counter
 #votes = Counter(candidate)
 
@@ -30,7 +29,7 @@ with open(csvpath) as csvfile:
 #candidatelist = list(votes.keys())
 #votecount = list(votes.values()) 
 
-#ok so this is what I originally did. We aren't allowed to use imported libraries for this assignment, but I'll just leave this here
+#ok so this is what I originally did. We aren't allowed to use imported modules for this assignment, but I'll just leave this here
 
 #------------------------------------------------------------
 
@@ -42,37 +41,34 @@ for x in range(len(candidate)):
         candidatelist.append(candidate[x])
 
 #use comprehensions! I originally used a for loop but should get into the practice of list comprehensions
-#then set the initial values to zero
-votecount = [0 for x in candidatelist]
-
 #for y in range(len(candidatelist)):
-#        votecount.append(int(0))    
-        
-#ok this took some thinking
-#we use nested for loop to add a vote counter every time the candidate name appears in the original "candidate" list
+#        votecount.append(int(0))   
+#set the initial values to zero
+votecount = [0 for x in candidatelist]
+     
+#nested for loop to add a vote counter every time the candidate name appears in the original "candidate" list
 for x in range(len(candidate)):
     for y in range(len(candidatelist)):
         if candidate[x] == candidatelist[y]:
             votecount[y] += 1
 
-
 #we create a list with the calculated percentage of votes (comprehensions!)
-#also you this format to present percentage values with 3 decimal places
+#also you can format to present values with 3 decimal places
 votepercentages = ["{:.3f}".format(i/totalvotes*100) for i in votecount]
 
-#just printing text
+#print text
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {totalvotes}")
 print("-------------------------")
 
-#print out the text for each candidate
+#print out the results for each unique candidate
 for i in range(len(candidatelist)):
     print(f"{candidatelist[i]}: {votepercentages[i]}% ({votecount[i]})")
 
 print("-------------------------")  
 
-#find the vote winner
+#find the vote winner and print
 for i in range(len(candidatelist)): 
     if max(votecount) == votecount[i]:
         print(f"Winner: {candidatelist[i]}")
